6d7d520b9b6cbcbb6fea4344898e6452aca00111
kandys24 <ccumbica24@gmail.com>
2024-11-05 00:28:53 +0100

    made some chang

--- src/Banner.js
+++ src/Banner.js
@@ -1,36 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './Banner.css';
 import candyBar from './img/banner/CandyBar.png';
-import Modal from './Modal';
 
 const Banner = () => {
-    const [current, setCurrent] = useState(0); // Index of the current image
-    const images = [candyBar, candyBar, candyBar]; // Array of images
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrent(prevImage => (prevImage + 1) % images.length);
-        }, 7000);
-    
-        return () => clearInterval(intervalId);
-    }, []);
-  
-    const previousImage = () => {
-        setCurrent(current === 0 ? images.length - 1 : current - 1);
-    };
-  
-    const nextImage = () => {
-        setCurrent(current === images.length - 1 ? 0 : current + 1);
-    };
-
-    const handleOpenModal = () =>{
-        setIsModalOpen(true);
-    }
-
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
   
     return (
         <>
@@ -42,9 +14,6 @@ const Banner = () => {
                     <div className='banner__box__tittle'><h1>Tropical Snack Bar</h1></div>
                 </div>
             </div>
-            {isModalOpen && (
-                <Modal closeModal={closeModal} />
-            )}
         </>
     );
   };
